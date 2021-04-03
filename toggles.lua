0149cc4a67d65ff4525283f02b4d2917154e2ff1
Dank <76633816+danktheskid@users.noreply.github.com>
2021-04-03 15:27:20 -0400

    Create toggles.lua

---
+++ toggles.lua
@@ -0,0 +1,11 @@
+Toggle rep display VVVVV
+game:service'ReplicatedStorage'.Events.Settings:FireServer("Rep", "off")
+game:service'ReplicatedStorage'.Events.Settings:FireServer("Rep", "on")
+
+--
+Toggle Streak display VVVV
+game:service'ReplicatedStorage'.Events.Settings:FireServer("Streak", "off")
+game:service'ReplicatedStorage'.Events.Settings:FireServer("Streak", "on")
+--
+
+(Could be used for automatic streak/rep display off when you join game)
